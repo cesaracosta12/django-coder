c2f348b92d75095d421412947d1e8d7c752fbd34
CesarAcosta <ocesaracosta@gmail.com>
2024-06-05 00:57:17 -0300

    martes noche

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,9 +120,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    BASE_DIR /'static',
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR /'static',
+# )
+# STATICFILES_DIRS = (
+#     os.path.join(os.path.dirname(__file__),'static'),
+# )
 
 # List of finder classes that know how to find static files in
 # various locations.
